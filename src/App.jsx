1d36c8fae9e78e880ddc826b520e068439063ae6
Luciano Mollar <mollar.luciano@gmail.com>
2024-08-18 20:31:44 -0300

    GA

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-
 import './App.css'
+import React, { useEffect } from 'react';
 import Header from './components/Header'
 import Nav from './components/Nav'
 import Main from './components/Main'
 import Footer from './components/Footer'
+import ReactGA from "react-ga4";
 
 function App() {
+
+  useEffect(()=>{
+    ReactGA.initialize(import.meta.env.VITE_GA_ID);
+    ReactGA.send({ hitType: "pageview", page: "/", title: "Home" });
+  },[]);
 
   return (
     <>
